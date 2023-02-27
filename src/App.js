738cd5282042187ce911d03e3b8d870b7269e675
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-02-27 19:56:52 +0530

    Footer Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
+import { useState } from 'react';
+import Footer from './components/Footer/Footer';
+
 function App() {
+  const [category, setCategory] = useState('General');
   return (
     <div className="App">
-    <Navbar />
+    <Navbar setCategory={setCategory}/>
+      
+    <Footer />
     </div>
   );
 }
